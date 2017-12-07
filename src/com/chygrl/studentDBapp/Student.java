2a7dec1525dd7c79f6e5e67934361d80c4c5d741
ChyGrl <32235891+ChyGrl@users.noreply.github.com>
2017-12-07 00:29:55 -0500

    12/6 changes

--- src/com/chygrl/studentDBapp/Student.java
+++ src/com/chygrl/studentDBapp/Student.java
@@ -7,79 +7,75 @@ public class Student {
 	private String lastName;
 	private String gradeLevel;
 	private String studentId;
-	private String courses = null;
+	private String courses;
 	private int tuitionBalance;
-	private int costOfCourse = 600;
+	private static int costOfCourse = 600;
 	private static int id = 10001;
-	
-	//Constructor to prompt name and year
+
+	// Constructor to prompt name and year
 	public Student() {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter student first name: ");
 		this.firstName = in.nextLine();
-		
+
 		System.out.println("Enter student last name: ");
 		this.lastName = in.nextLine();
-		
 
-		System.out.println("Enter student grade level: ");
+		System.out.print("1 - Freshman\n2 - Sophmore\n3- Junior\n4- Senior\nEnter student class level:");
 		this.gradeLevel = in.nextLine();
-		System.out.println(firstName + " " + lastName + ":" + " " + gradeLevel);
+		// System.out.println(firstName + " " + lastName + ":" + " " + gradeLevel);
+		
+		
 		
 		setStudentId();
-		
+
 		System.out.println(firstName + " " + lastName + " " + gradeLevel + " " + studentId);
-		
-//		this.studentId = randomID(8);
-//		System.out.println("Your Student ID is:" + studentId);
-//	
-	
+
 	}
-	
-	
-	//Generate ID using a Random password code
-	//Does NOT meet the criteria set by the scenario; 
-	//5 digit unique ID with the first number being thier grade level
-//	private String randomID(int length) {
-//		String studentIdSet = "1234567890";
-//		char[] ID = new char[length];
-//		for (int i = 0; i <length; i++) {
-//			int rand = (int) (Math.random() * studentIdSet.length());
-//			ID [i] = studentIdSet.charAt(rand);
-//		}
-//		return new String(ID);
-//	}
-//	
-	//Generate an ID in a more 'controlled fashion.'
-	
+
+	// Generate ID using a Random password code
 	private void setStudentId() {
-		//grade level +id
-		id++; 	
+		// grade level +id
+		
+		id++;
 		this.studentId = gradeLevel + " " + id;
-	} 
-	
-	//Enroll in courses
-	public void enroll() {
-		//create a loop
-		System.out.println("Enter to enroll Q to quit):");
-		Scanner in = new Scanner(System.in);
-		String course = in.nextLine();
-		if (course != "Q") {
-			courses = courses + "\n" + course;
-			tuitionBalance = tuitionBalance + costOfCourse;
-			
-		}
-	System.out.println("Enrolled in" + courses);
-	System.out.println(tuitionBalance);
 	}
-	
-	
-	
-	//View balance  
-	
-	
-	//Pay tuition
-	
-	//Show status
-	
+
+	// Enroll in courses
+	public void enroll() {
+		do {
+			// create a loop
+			System.out.print("Enter Course name to enroll (Press Q to quit):");
+			Scanner in = new Scanner(System.in);
+			String course = in.nextLine();
+			if (!course.equals("Q")) {
+				courses = courses + "\n" + course;
+				tuitionBalance = tuitionBalance + costOfCourse;
+
+			} else {
+				break;
+			}
+		} while (1 != 0);
+
+		System.out.println("Enrolled in: " + courses);
+	}
+
+	// View balance
+
+	public void viewBalance() {
+		System.out.println("Your Balance is: " + tuitionBalance);
+	}
+
+	// Pay tuition
+	public void payTuition() {
+		viewBalance();
+		System.out.print("Enter your payment: $");
+		Scanner in = new Scanner(System.in);
+		int payment = in.nextInt();
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thank you for your payment of $ :" + payment);
+		viewBalance();
+	}
+	// Show status
+
 }
